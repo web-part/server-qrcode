81d530e3bff9d5d26f37c0f00e18bb3b18d41f01
micty <mictydeng@qq.com>
2022-05-26 16:17:34 +0800

    2.0.0

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
 
-
-const Path = require('./modules/Path');
-const Response = require('./modules/Response');
+const qr = require('qr-image');
 
 
 
 module.exports = {
-
     /**
     * 启动二维码服务。
     * @param {*} app 
     * @param {object} opt 
     *   opt = {
-    *       host: 'localhost',  //如果不传，则默认为 `localhost`。
-    *       port: 3001,         //端口号。
-    *       size: 10,           //二维码的大小。
-    *       path: '/qrcode',    //页面路径。
+    *       path: '/qrcode',    //必选，页面路径。
+    *       size: 5,            //可选，二维码的大小。
+    *       margin: 4,          //可选，二维码图像四周的空白。
     *   };
-    * @returns { path, size, url, }
+    * @returns { path, }
     */
     start(app, opt) {
-        let { size, } = opt;
-        let { path, regexp, url, } = Path.get(opt);
+        let { path, } = opt;
 
-        //针对 `/qr` 的根目录路由。
+        //针对 `/` 的根目录路由。
+        //支持直接指定目标 url，
+        //如`http://localhost:3001/qrcode?size=10&margin=2&url=http%3A%2F%2Fgoogle.com`。
         app.get(path, (req, res) => {
-            Response.send(res, {
+            let { url, size, margin, } = req.query;
+
+            url = url || `http://${req.headers.host}`;
+            size = Number(size || opt.size) || 5;           //大小不能为 0。
+            margin = Number(margin || opt.margin) || 0;     //空白可以为 0。
+
+            let image = qr.image(url, {
+                'type': 'png',
                 'size': size,
-                'url': url,
+                'margin': margin,
             });
+
+            res.setHeader('Content-type', 'image/png');
+            image.pipe(res);
         });
 
 
-        //针对如 `/qr/a/b/c` 这样的任意路由。
-        app.get(regexp, (req, res) => {
-            let dir = req.url.slice(path.length);
-          
-            Response.send(res, {
-                'size': size,
-                'url': `${url}${dir}`,
-            });
-
-        });
-
-        return {
-            path,
-            size,
-            url,
-        };
+        return { path, };
 
     },
 
